9b47206c0bc585c8eaf75492c8e9ace496f94a3f
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2020-03-05 18:03:56 +0200

    initial model of coordiantes initialization form

--- src/client/index.js
+++ src/client/index.js
@@ -50,160 +50,169 @@ function tick(event) {
 }
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZmlyc3RzdGVmIiwiYSI6ImNrNzRneHkzbTBpaDQzZnBkZDY3dXRjaDQifQ.g6l-GFeBB2cUisg6MqweaA';
-let map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [27.598505,47.162098],
-    zoom: 20
-});
+
+
+// pixels the map pans when the up or down arrow is clicked
+const deltaDistance = 500;
+
+// degrees the map rotates when the left or right arrow is clicked
+const deltaDegrees = 25;
+
+function easing(t) {
+    return t * (2 - t);
+}
+
+
 
 var mySource;
-
-map.on('load', function() {
-    map.loadImage(
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png',
-        function(error, image) {
-            if (error) throw error;
-            map.addImage('cat', image);
-            map.addSource('point', {
-                'type': 'geojson',
-                'data': {
-                    'type': 'FeatureCollection',
-                    'features': [
-                        {
-                            'type': 'Feature',
-                            'geometry': {
-                                'type': 'Point',
-                                'coordinates': [0, 0]
+var map ;
+function setMap(lat=27.598505, long=47.162098){
+    playerPos[0]=lat;
+    playerPos[1]=long;
+    map =new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [playerPos[0],playerPos[1]],
+        zoom: 20
+    });
+    map.on('load', function() {
+        map.loadImage(
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png',
+            function(error, image) {
+                if (error) throw error;
+                map.addImage('cat', image);
+                map.addSource('point', {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'FeatureCollection',
+                        'features': [
+                            {
+                                'type': 'Feature',
+                                'geometry': {
+                                    'type': 'Point',
+                                    'coordinates': [0, 0]
+                                }
                             }
-                        }
-                    ]
-                }
-            });
-            map.addLayer({
-                'id': 'points',
-                'type': 'symbol',
-                'source': 'point',
-                'layout': {
-                    'icon-image': 'cat',
-                    'icon-size': 0.25
-                }
-            });
-        }
-    );
-
-    // pixels the map pans when the up or down arrow is clicked
-    const deltaDistance = 500;
-
-    // degrees the map rotates when the left or right arrow is clicked
-    const deltaDegrees = 25;
-
-    function easing(t) {
-        return t * (2 - t);
-    }
-
-    map.getCanvas().focus();
-
-    map.getCanvas().addEventListener(
-        'keydown',
-        function(e) {
-            e.preventDefault();
-            isRunning = true;
-            if (e.which === 38) {
-            // up
-                map.panBy([0, -deltaDistance], {
-                    easing: easing
+                        ]
+                    }
                 });
-
-            } else if (e.which === 40) {
-            // down
-                map.panBy([0, deltaDistance], {
-                    easing: easing
-                });
-            } else if (e.which === 37) {
-            // left
-                map.easeTo({
-                    bearing: map.getBearing() - deltaDegrees,
-                    easing: easing
-                });
-            } else if (e.which === 39) {
-            // right
-                map.easeTo({
-                    bearing: map.getBearing() + deltaDegrees,
-                    easing: easing
+                map.addLayer({
+                    'id': 'points',
+                    'type': 'symbol',
+                    'source': 'point',
+                    'layout': {
+                        'icon-image': 'cat',
+                        'icon-size': 0.25
+                    }
                 });
             }
-        },
-        true
-    );
-});
+        );
 
 
-setInterval( function(){
+        map.getCanvas().focus();
 
-    var features = map.queryRenderedFeatures(
-        {/*sourceLayer: ["road", "building"]*/}); // This is where I get building
+        map.getCanvas().addEventListener(
+            'keydown',
+            function(e) {
+                e.preventDefault();
+                isRunning = true;
+                if (e.which === 38) {
+                // up
+                    map.panBy([0, -deltaDistance], {
+                        easing: easing
+                    });
 
-    features.forEach(function(feature){
-        //console.log(feature.geometry);  // feature.geometry getter returns building shape points (basement)
-        //console.log(feature);
+                } else if (e.which === 40) {
+                // down
+                    map.panBy([0, deltaDistance], {
+                        easing: easing
+                    });
+                } else if (e.which === 37) {
+                // left
+                    map.easeTo({
+                        bearing: map.getBearing() - deltaDegrees,
+                        easing: easing
+                    });
+                } else if (e.which === 39) {
+                // right
+                    map.easeTo({
+                        bearing: map.getBearing() + deltaDegrees,
+                        easing: easing
+                    });
+                }
+            },
+            true
+        );
+    });
 
-        console.log(feature.id);
 
-        if (feature["id"] ==  229536321)
-        {
-            console.log('hai');
-            console.log(feature);
-        }
+    setInterval( function(){
 
-        if (feature.sourceLayer == "road" || feature["source-layer"] == "road") {
+        var features = map.queryRenderedFeatures(
+            {/*sourceLayer: ["road", "building"]*/}); // This is where I get building
 
-            let polygon = new createjs.Shape();
-            polygon.graphics.beginStroke("blue");
+        features.forEach(function(feature){
+            //console.log(feature.geometry);  // feature.geometry getter returns building shape points (basement)
+            //console.log(feature);
 
-            polygon.graphics.moveTo(-(playerPos[0]-feature.geometry.coordinates[0][0])*ZOOM + offsetx, (playerPos[1]-feature.geometry.coordinates[0][1])*ZOOM+ offsety);
+            console.log(feature.id);
 
-            //console.log(-(playerPos[0]-feature.geometry.coordinates[0][0])*ZOOM+ offsetx, (playerPos[1]-feature.geometry.coordinates[0][1])*ZOOM+ offsety);
+            if (feature["id"] ==  229536321)
+            {
+                console.log('hai');
+                console.log(feature);
+            }
 
-            feature.geometry.coordinates.forEach(function (point) {
-                polygon.graphics.lineTo(-(playerPos[0]-point[0])*ZOOM+ offsetx, (playerPos[1]-point[1])*ZOOM+ offsety);
-            });
+            if (feature.sourceLayer == "road" || feature["source-layer"] == "road") {
 
-            stage.addChild(polygon);
-        }
-        else if (feature.sourceLayer == "building"){
-            let polygon = new createjs.Shape();
-            polygon.graphics.beginStroke("red");
+                let polygon = new createjs.Shape();
+                polygon.graphics.beginStroke("blue");
 
-            feature.geometry.coordinates.forEach(function (building){
-                let buildingx=-(playerPos[0] - building[0][0]) * ZOOM + offsetx;
-                let buildingy = (playerPos[1] - building[0][1]) * ZOOM + offsety;
-                polygon.graphics.moveTo(buildingx, buildingy);
-                building.forEach(function (point) {
+                polygon.graphics.moveTo(-(playerPos[0]-feature.geometry.coordinates[0][0])*ZOOM + offsetx, (playerPos[1]-feature.geometry.coordinates[0][1])*ZOOM+ offsety);
+
+                //console.log(-(playerPos[0]-feature.geometry.coordinates[0][0])*ZOOM+ offsetx, (playerPos[1]-feature.geometry.coordinates[0][1])*ZOOM+ offsety);
+
+                feature.geometry.coordinates.forEach(function (point) {
                     polygon.graphics.lineTo(-(playerPos[0]-point[0])*ZOOM+ offsetx, (playerPos[1]-point[1])*ZOOM+ offsety);
                 });
-            });
 
-            stage.addChild(polygon);
-        }
+                stage.addChild(polygon);
+            }
+            else if (feature.sourceLayer == "building"){
+                let polygon = new createjs.Shape();
+                polygon.graphics.beginStroke("red");
+
+                feature.geometry.coordinates.forEach(function (building){
+                    let buildingx=-(playerPos[0] - building[0][0]) * ZOOM + offsetx;
+                    let buildingy = (playerPos[1] - building[0][1]) * ZOOM + offsety;
+                    polygon.graphics.moveTo(buildingx, buildingy);
+                    building.forEach(function (point) {
+                        polygon.graphics.lineTo(-(playerPos[0]-point[0])*ZOOM+ offsetx, (playerPos[1]-point[1])*ZOOM+ offsety);
+                    });
+                });
+
+                stage.addChild(polygon);
+            }
 
 
-        var g = new createjs.Graphics();
-        g.setStrokeStyle(1);
-        g.beginStroke(createjs.Graphics.getRGB(0,0,0));
-        g.beginFill(createjs.Graphics.getRGB(0,255,0));
-        g.drawCircle(0,0,3);
+            var g = new createjs.Graphics();
+            g.setStrokeStyle(1);
+            g.beginStroke(createjs.Graphics.getRGB(0,0,0));
+            g.beginFill(createjs.Graphics.getRGB(0,255,0));
+            g.drawCircle(0,0,3);
 
-        var s = new createjs.Shape(g);
-        s.x = 5000;
-        s.y = 10000;
-        s.name = "playerdot";
+            var s = new createjs.Shape(g);
+            s.x = 5000;
+            s.y = 10000;
+            s.name = "playerdot";
 
-        stage.addChild(s);
+            stage.addChild(s);
 
 
 
-        //console.log(feature.properties.height); // this is the building height
-        //console.log(feature.properties.min_height); // this is the building part elevation from groung (e.g. a bridge)
-    });
-}, 1000);
+            //console.log(feature.properties.height); // this is the building height
+            //console.log(feature.properties.min_height); // this is the building part elevation from groung (e.g. a bridge)
+        });
+    }, 1000);
+
+}
