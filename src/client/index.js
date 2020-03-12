3b26156d91a34b811f0ca43852cb0d251093ea78
blox-dev <bloxorz51gaming@gmail.com>
2020-03-12 19:03:34 +0200

    Improved front page. Fixed form and redirect link

--- src/client/index.js
+++ src/client/index.js
@@ -7,21 +7,24 @@ var lat;
 
 
 function getFormInput() {
-     longitude = document.getElementById("longitude").value;
-     lat = document.getElementById("latitude").value;
-    if (formValidation(lat, longitude)) {
+     longitude = document.getElementById("longitude_button").value;
+     lat = document.getElementById("latitude_button").value;
+    var ok=formValidation(lat, longitude);
+     if (ok) {
         //setMap(longitude, lat);
         //init();
+        document.getElementById("form").target="_self";
+        document.getElementById("form").action= "game.html";
+        document.getElementById("form").method="GET";
         document.getElementById("formMessage").innerHTML = "Enter coordinates: ";
-        //showForm(true);
     } else {
         let str = "Enter valid coordinates: ";
         document.getElementById("formMessage").innerHTML = str.fontcolor("red");
     }
+    return ok;
 }
 
 function formValidation(lat, longitude) {
-    console.log(lat);
     let val2 = parseFloat(longitude);
     let val1 = parseFloat(lat);
     console.log(val1 + " " + val2);
