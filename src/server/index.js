a86f60d12eea94c737a1aee788ce96a4f392ed51
firststef <firstbossspike@gmail.com>
2020-04-16 20:03:19 +0300

    Added server sync logic

--- src/server/index.js
+++ src/server/index.js
@@ -2,40 +2,48 @@ const url = require('url');
 const http = require('http');
 var io = require('socket.io')(80);
 
-var playerCoordinates = [[0,0], [0,0]];
+var playerMap = Map();
+//player Map -> has socket and coordinates
 
-var firstClient;
-var secondClient;
+function distance(point1, point2){
+    return Math.sqrt(Math.pow((point1[0] - point2[0]), 2) + Math.pow((point1[1] - point2[1]), 2));
+}
+
+let radius = 0.5;
+function sendOtherPlayerCoordinates(){
+    let dist;
+    playerMap.forEach((firstPlayerObj, firstPlayerId) => {
+        //search through other players
+
+        let otherPlayers = [];
+        playerMap.forEach((otherPlayerObj, otherPlayerId) => {
+            dist = distance(otherPlayerCoords.coordinates, firstPlayerCoords.coordinates);
+            if (firstPlayerId !== otherPlayerId && !isNaN(dist) && dist < radius){
+                otherPlayers.push(otherPlayerObj.socket);
+            }
+        });
+
+        if (otherPlayers.length){
+            otherPlayers.forEach((socket) => {
+                socket.emit('other_player', 'send');
+            });
+        }
+    });
+}
+
+setInterval(sendOtherPlayerCoordinates, 1000);
 
 io.on('connection', function (socket) {
+    playerMap.set(socket.id, {coordinates: [0,0], socket: socket});
+
     socket.on('coordonate', function (obj) {
         let parsedObj = JSON.parse(obj);
 
-        if (firstClient === undefined)
-            firstClient = socket.id;
-        else
-            secondClient = socket.id;
-
-        if (firstClient === socket.id)
-        {
-            playerCoordinates[0] = parsedObj.coordinates;
-            console.log('Player1', playerCoordinates[0]);
-        }
-        else if (secondClient === socket.id){
-            playerCoordinates[1] = parsedObj.coordinates;
-        }
-
-        if (firstClient === socket.id)
-        {
-            socket.emit('other_player', JSON.stringify(playerCoordinates[1]));
-        }
-        else if (secondClient === socket.id) {
-            socket.emit('other_player', JSON.stringify(playerCoordinates[0]));
-        }
+        playerMap.set(socket.id, {coordinates: parsedObj.coordinates, socket:socket});
     });
 
     socket.on('disconnect', function () {
-
+        playerMap.delete(socket.id);
     });
 });
 
