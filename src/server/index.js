c95c3c5f3ff4e9a503c6cb4e07cff0d41564fa50
firststef <firstbossspike@gmail.com>
2020-04-22 02:48:25 +0300

    Removed test

--- src/server/index.js
+++ src/server/index.js
@@ -44,12 +44,6 @@ function serverHandler(req, res) {
     const parsedUrl = url.parse(req.url, true);
     const resource = config.resources[parsedUrl.pathname];
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write("<h1>Continous integration works</h1>");
-    res.end();
-    return;
-
     if (resource === undefined){
         logToFile("Path not registered:" + req.url);
         res.writeHead(404);
